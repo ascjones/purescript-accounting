4c4ad87147f9c455fc9b2e182f6c9dfef09e1f0d
Andrew Jones <ascjones@gmail.com>
2016-01-27 21:34:10 +0000

    Add unit test for formatMoney

--- src/Accounting/Accounting.purs
+++ src/Accounting/Accounting.purs
@@ -4,6 +4,23 @@ import Prelude
 
 import Data.Function
 
+type CurrencySettings =
+  { symbol    :: String
+  , format    :: String
+  , decimal   :: Char
+  , thousand  :: Char
+  , precision :: Int
+  }
+
+defaultCurrencySettings :: CurrencySettings
+defaultCurrencySettings =
+  { symbol : "$"
+  , format : "%s%v"
+  , decimal : '.'
+  , thousand : ','
+  , precision : 2
+  }
+
 formatMoney :: Number -> String -> Int -> Char -> Char -> String -> String
 formatMoney n sym prec thou dec fmt = runFn6 formatMoneyImpl n sym prec thou dec fmt
 
--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,20 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Test.Unit (test, runTest)
+import Test.Unit.Assert as Assert
+
+import Accounting (formatMoney)
+
+-- main = runTest do
+--   test "Service Tests" do
+--     assertFn "Nutcracker is missing from inventory" $
+--       checkInventory "Nutcracker"
+--     assertFn "Spinning Top is missing from inventory" $
+--       checkInventory "Spinning Top"
+
+-- main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = runTest do
+  test "Format Money" do
+    Assert.assert "Can format number as currency" $ (formatMoney 10000.0 "£" 2 ',' '.' "%s%v") == "£10,000.00" 
