f5cf40b0bb2b8f5d38f144aaeebfa9ca1159f125
Andrew Jones <ascjones@gmail.com>
2016-01-29 18:53:16 +0000

    Passing test for format money

--- src/Accounting/Accounting.js
+++ src/Accounting/Accounting.js
@@ -4,5 +4,5 @@
 // module Accounting
 
 exports.formatMoneyImpl = function (number, symbol, precision, thousand, decimal, format) {
-  require('accounting').formatMoney(number, symbol, precision, thousand, decimal, format);
+  return require('accounting').formatMoney(number, symbol, precision, thousand, decimal, format);
 };
--- test/Main.purs
+++ test/Main.purs
@@ -2,19 +2,17 @@ module Test.Main where
 
 import Prelude
 
-import Test.Unit (test, runTest)
+import Control.Monad.Eff.Class (liftEff)
+
+import Test.Unit (TestUnit(), test, runTest)
 import Test.Unit.Assert as Assert
+import Test.Unit.Console
 
 import Accounting (formatMoney)
 
--- main = runTest do
---   test "Service Tests" do
---     assertFn "Nutcracker is missing from inventory" $
---       checkInventory "Nutcracker"
---     assertFn "Spinning Top is missing from inventory" $
---       checkInventory "Spinning Top"
-
--- main :: forall e. Eff (console :: CONSOLE | e) Unit
+-- main :: forall eff. Eff (timer :: TIMER, avar :: AVAR, testOutput :: TESTOUTPUT | eff)
 main = runTest do
   test "Format Money" do
-    Assert.assert "Can format number as currency" $ (formatMoney 10000.0 "£" 2 ',' '.' "%s%v") == "£10,000.00" 
+    let result = formatMoney 10000.0 "£" 2 ',' '.' "%s%v"
+    liftEff $ consoleLog result
+    Assert.assert "Formats number as currency" $ result == "£10,000.00"
